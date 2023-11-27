e920b24c3f8d5ea03558fd8edaec5a19fb54b615
Bravax <xavierlobez@hotmail.fr>
2023-11-27 14:17:15 +0100

    express initialization

---
+++ test/movies.test.js
@@ -0,0 +1,12 @@
+const request = require("supertest");
+const app = require("../app");
+
+describe("GET /api/movies", () => {
+    it("should return all movies", async () => {
+        const response = await request(app).get("/api/movies");
+        console.log(response);
+
+        expect(response.headers["content-type"]).toMatch(/json/);
+        expect(response.status).toEqual(200)
+    });
+});
